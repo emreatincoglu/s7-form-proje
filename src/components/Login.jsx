f33327b99960e75e63be5ee7917b38fbbb34db86
Emre Atincoglu <emreatincoglu2@gmail.com>
2025-12-16 16:22:04 +0300

    First commit
    Form dogru bir sekilde olusturuldu

---
+++ src/components/Login.jsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  FormFeedback,
+} from 'reactstrap';
+import { useHistory } from 'react-router-dom';
+
+import axios from 'axios';
+
+const initialForm = {
+  email: '',
+  password: '',
+  terms: false,
+};
+
+const errorMessages = {
+  email: 'Please enter a valid email address',
+  password: 'Password must be at least 4 characters long',
+};
+
+export default function Login() {
+  const [form, setForm] = useState(initialForm);
+  const [errors, setErrors] = useState(errorMessages);
+  const [isValid, setIsValid] = useState(false);
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    let { name, value, type } = event.target;
+    value = type === 'checkbox' ? event.target.checked : value;
+    setForm({ ...form, [name]: value });
+
+    if (name === 'email') {
+      if (value.includes('@') && value.includes('.')) {
+        setErrors({ ...errors, email: '' });
+      } else {
+        setErrors({ ...errors, email: 'Please enter a valid email address' });
+      }
+    }
+
+    if (name === 'password') {
+      if (value.length < 4) {
+        setErrors({
+          ...errors,
+          password: 'Password must be at least 4 characters long',
+        });
+      } else {
+        setErrors({ ...errors, password: '' });
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (errors.email === '' && errors.password === '' && form.terms === true) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [form, errors]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (isValid === false) {
+      return 'hata';
+    } else {
+      axios
+        .get('https://6540a96145bedb25bfc247b4.mockapi.io/api/login')
+        .then((res) => {
+          const user = res.data.find(
+            (item) => item.password == form.password && item.email == form.email
+          );
+          if (user) {
+            setForm(initialForm);
+            history.push('/main');
+          } else {
+            history.push('/error');
+          }
+        });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input
+          id="exampleEmail"
+          name="email"
+          placeholder="Enter your email"
+          type="email"
+          onChange={handleChange}
+          value={form.email}
+          invalid={errors.email !== ''}
+        />
+        {errors.email && <FormFeedback>{errors.email}</FormFeedback>}
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          id="examplePassword"
+          name="password"
+          placeholder="Enter your password "
+          type="password"
+          onChange={handleChange}
+          value={form.password}
+          invalid={errors.password !== ''}
+        />
+        {errors.password && <FormFeedback>{errors.password}</FormFeedback>}
+      </FormGroup>
+      <FormGroup check>
+        <Input
+          id="terms"
+          name="terms"
+          checked={form.terms}
+          type="checkbox"
+          onChange={handleChange}
+        />{' '}
+        <Label htmlFor="terms" check>
+          I agree to terms of service and privacy policy
+        </Label>
+      </FormGroup>
+      <FormGroup className="text-center p-4">
+        <Button color="primary" disabled={!isValid}>
+          Sign In
+        </Button>
+      </FormGroup>
+    </Form>
+  );
+}
